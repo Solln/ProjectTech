46c181375f3a0db81e3457d53a63caf31e54a2b3
Darren Lowe <yxb15183@strath.ac.uk>
2018-05-21 21:33:43 +0100

    Added half of a simple nav bar, still experimenting atm

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Darren's First Express App Hosted on an EB Instance" });
+  res.render('index', { title: "Darren's First Express App Hosted on an EB Instance" , welcome: "Welcome to the Index page"});
 });
 
 router.get('/testPage1', function(req, res, next) {
-    res.render('testPage1', { title: "Darren's First Express App Hosted on an EB Instance" });
+    res.render('testPage1', { title: "Darren's First Express App Hosted on an EB Instance", welcome: "Welcome to Test Page 1" });
 });
 
 module.exports = router;
