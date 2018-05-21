05750b56af6a2e1fdabbca93f92180029f17efdf
Darren Lowe <yxb15183@strath.ac.uk>
2018-05-21 17:54:32 +0100

    Test re-deploy to EB

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Darren's First Express App Hosted on an EB Instance" });
 });
 
 module.exports = router;
