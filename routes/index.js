7d83577cf95ef89d7d4b313736aaec07b4999f5a
Darren Lowe <yxb15183@strath.ac.uk>
2018-05-25 22:21:42 +0100

    Added some more routing and dynamic updates

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,23 @@ router.get('/aboutMe', function(req, res, next) {
 });
 
 router.get('/projectSuggestion', function(req, res, next) {
-    res.render('projectSuggestion', { title: "Project Suggestions", welcome: "Welcome to Project Suggestions" });
+    res.render('projectSuggestion', { title: "Mythic Argus Wipe Fest", welcome: "Welcome to Mythic Argus Wipe Fest" });
 });
+
+router.get('/tester', function(req, res, next) {
+    res.render('tester');
+});
+
+router.get('/projectTemp', function(req, res, next) {
+    var cat = req.param('cat');
+    var project = req.param('project')
+    res.render('projects/projectTemp', {catagory: cat, project:project});
+});
+
+router.get('/projectSel', function(req, res, next) {
+    var cat = req.param('cat');
+    res.render('projects/projectSelTem', {catagory: cat});
+});
+
 
 module.exports = router;
